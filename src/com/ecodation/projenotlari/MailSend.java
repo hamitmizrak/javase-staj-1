cc4366336437548fd28580fa18ff0b7c8a348c91
HamitMizrak <hamitmizrak@gmail.com>
2021-07-05 11:52:06 +0300

    Mail Send KAynakça

---
+++ src/com/ecodation/projenotlari/MailSend.java
@@ -0,0 +1,9 @@
+package com.ecodation.projenotlari;
+
+public class MailSend {
+	// kaynakçalar
+	// https://www.baeldung.com/java-email
+	// https://www.tutorialspoint.com/java/java_sending_email.htm
+	// https://www.javatpoint.com/example-of-sending-email-using-java-mail-api
+	// https://mkyong.com/java/java-how-to-send-email/
+}
